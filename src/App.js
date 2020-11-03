ec8d9e78aadf07d28fcb0d258b19ef55865af806
Luca <lucakanyo@protonmail.com>
2020-11-02 20:06:20 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Test Button</Button>
-      </header>
+        <Navbar/>
+        <Button>Bootstrap!</Button>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar(){
+    return (
+        <div>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About</li>
+        </div>
+    )
+}
+
+export default Navbar;

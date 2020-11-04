28bf6e18d0f4f6333d7e1ae377ba99c7323bc24a
Luca <lucakanyo@protonmail.com>
2020-11-03 19:47:06 -0500

    filled in about file with placeholder text

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
 import ProjectPage from './components/ProjectPage';
 import Intro from './components/Intro'
+import About from './components/About'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar/>
         <Intro/>
         <ProjectPage/>
+        <About/>
     </div>
   );
 }

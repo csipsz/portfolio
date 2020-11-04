b4559db8ee8ade837f75c8daa34da53b238fb463
Luca <lucakanyo@protonmail.com>
2020-11-03 19:45:10 -0500

    added mock data to intro component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
 import ProjectPage from './components/ProjectPage';
+import Intro from './components/Intro'
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Intro/>
         <ProjectPage/>
     </div>
   );
---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Intro = () => {
+    return (
+        <div>
+            This is the intro to my website's main page
+        </div>
+    )
+}
+
+export default Intro

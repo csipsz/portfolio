44f5c1bd34f23a97defdf461ee7a5447046f5082
Luca <lucakanyo@protonmail.com>
2020-11-03 19:31:53 -0500

    added project and projectlist component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
+import ProjectList from './components/ProjectList';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
-        <Button>Bootstrap!</Button>
+        <ProjectList/>
     </div>
   );
 }
---
+++ src/components/ProjectList.js
@@ -0,0 +1,13 @@
+import Project from './Project'
+
+function ProjectList(){
+    return (
+        <div>
+            <Project/>
+            <Project/>
+            <Project/>
+        </div>
+    )
+}
+
+export default ProjectList

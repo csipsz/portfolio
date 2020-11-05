32ee2475777366cb220447d1017727ab746ad8a5
Luca <lucakanyo@protonmail.com>
2020-11-04 21:21:20 -0500

    updated npm and added react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import Navbar from './components/Navbar';
 import ProjectPage from './components/ProjectPage';
 import Intro from './components/Intro'
 import About from './components/About'
+import { Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
         <Navbar/>
         <Intro/>
         <ProjectPage/>
         <About/>
+        </BrowserRouter>
     </div>
   );
 }
